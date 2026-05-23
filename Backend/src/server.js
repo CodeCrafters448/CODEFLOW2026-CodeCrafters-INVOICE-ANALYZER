6d9a49790a0d7cd8dab3beb8e1ffd37f9c13d464
Ajoy563 <dasajoy661@gmail.com>
2026-05-23 13:52:22 +0530

    update

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+const { connectDB } = require("./config/db");
+const uploadRoutes = require("./routes/uploadRoutes");
+
+connectDB();
 
 const app = express();
 
@@ -10,7 +16,9 @@ app.get("/", (req, res) => {
     res.send("Backend Running Successfully");
 });
 
-const PORT = 5000;
+app.use("/upload", uploadRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
